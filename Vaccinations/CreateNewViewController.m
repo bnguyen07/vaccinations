385fa56f59c70c0967d5c8bad2fac610b90e994c
heoiu87 <brian_2447@hotmail.com>
2013-11-24 09:31:42 -0800

    Just fix the gender in Create New Patient. Just move the _genderString init to the right place. Move on to the birthDate. It need to be fixed a little bit.

--- Vaccinations/CreateNewViewController.m
+++ Vaccinations/CreateNewViewController.m
@@ -83,9 +83,7 @@ NSString *kPostURL;
     self.title = @"Create New";
    kPostURL = [[NSString alloc] initWithFormat:@"http://%@/postNewRecord.php", gServerIp];
    NSLog(@"kPostURL: %@", kPostURL);
-    _genderString = [[NSString alloc] initWithString:[_gender titleForSegmentAtIndex:[_gender selectedSegmentIndex]]];
-    
-     NSLog(@"Physician got from Login page: %@", _physician_id);
+   NSLog(@"Physician got from Login page: %@", _physician_id);
     
     
 }
@@ -125,6 +123,7 @@ NSString *kPostURL;
         
         [postString appendString:[NSString stringWithFormat:@"&%@=%@", kbirthdate, birthDate]];
         
+         _genderString = [[NSString alloc] initWithString:[_gender titleForSegmentAtIndex:[_gender selectedSegmentIndex]]];
         
         [postString appendString:[NSString stringWithFormat:@"&%@=%@", kgender, [[_genderString substringToIndex:1] capitalizedString]]];
         
