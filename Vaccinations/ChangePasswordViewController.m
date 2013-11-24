e5a111dc40efb9d5c7bdbe26050cf75a61202517
heoiu87 <brian_2447@hotmail.com>
2013-11-24 11:12:00 -0800

    Fixed Bug #238926 “Pop up incorrectly states problem when changing password”

--- Vaccinations/ChangePasswordViewController.m
+++ Vaccinations/ChangePasswordViewController.m
@@ -64,50 +64,61 @@ NSString *kGetUrlToChangePassword;
 }
 
 - (IBAction)changePassword:(id)sender {
-    
-    if (![[Password_New text] isEqualToString: [Reenter_NewPassword text]]) {
-        UIAlertView *passwordNotMatch = [[UIAlertView alloc] initWithTitle:@"Passwords Not Match" message:@"The new passwords don't match." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
-        [passwordNotMatch show];
-    } else if ([[Password_Current text] isEqualToString:[Password_New text]]) {
-        UIAlertView *passwordHasBeenUsed = [[UIAlertView alloc] initWithTitle:@"Passwords Has Been Used" message:@"The new password is the same as the current password." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
-        [passwordHasBeenUsed show];
-    }else{
-    
-        //Brian: Nov 06, 2013
-        //Create Register Patient User post string
-        
-        NSMutableString *postString = [NSMutableString stringWithString:kGetUrlToChangePassword];
-        [postString appendString:[NSString stringWithFormat:@"?%@=%@", kuser_id, user_id]];
-        
-        [postString appendString:[NSString stringWithFormat:@"&%@=%@", kpassword, [Password_Current text]]];
-        [postString appendString:[NSString stringWithFormat:@"&%@=%@", knew_password, [Password_New text]]];
-        
-        NSLog(@"%@",postString);
-        
-        [postString setString:[postString stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
-        
-        NSURL *url = [NSURL URLWithString:postString];
-        NSLog(@"This is the GET string for the Change Physician password: %@", url);
-        
-        NSString *postResult = [[NSString alloc] initWithContentsOfURL:url encoding:NSUTF8StringEncoding error:nil];
-        
-        if ([postResult isEqualToString:@"Cannot update your password! Please check your inputs."]) {
-            UIAlertView *failAlert = [[UIAlertView alloc] initWithTitle:@"Fail to update password" message:postResult delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
-            [failAlert show];
-            return;
-        } else {
-            
-            
-            UIAlertView *successfulAlert = [[UIAlertView alloc] initWithTitle:@"Successfully!" message:postResult delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
-            [successfulAlert show];
-            return;
-            
-            NSLog(@"Username has been created successfully.");
+    // Check if all required fields have been filled
+    if ([Password_Current.text  isEqual: @""] || [Password_New.text  isEqual: @""]
+        || [Reenter_NewPassword.text  isEqual: @""] ) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Alert" message:@"Please fill in all required fields." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [alert show];
+        return;
+    } else {
+       
+        // Check if new passwords match
+        if (![[Password_New text] isEqualToString: [Reenter_NewPassword text]]) {
+            UIAlertView *passwordNotMatch = [[UIAlertView alloc] initWithTitle:@"Passwords Not Match" message:@"The new passwords don't match." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+            [passwordNotMatch show];
         }
-        
+        // Check if new password is different to current password
+        else if ([[Password_Current text] isEqualToString:[Password_New text]]) {
+                UIAlertView *passwordHasBeenUsed = [[UIAlertView alloc] initWithTitle:@"Invalid input" message:@"The new password is the same as the current password." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+                [passwordHasBeenUsed show];
+        }
+        // When all the inputs seem right, then we connect to the database to update the password
+        else {
+            NSMutableString *postString = [NSMutableString stringWithString:kGetUrlToChangePassword];
+            [postString appendString:[NSString stringWithFormat:@"?%@=%@", kuser_id, user_id]];
+            
+            [postString appendString:[NSString stringWithFormat:@"&%@=%@", kpassword, [Password_Current text]]];
+            [postString appendString:[NSString stringWithFormat:@"&%@=%@", knew_password, [Password_New text]]];
+            
+            NSLog(@"%@",postString);
+            
+            [postString setString:[postString stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+            
+            NSURL *url = [NSURL URLWithString:postString];
+            NSLog(@"This is the GET string for the Change Physician password: %@", url);
+            
+            NSString *postResult = [[NSString alloc] initWithContentsOfURL:url encoding:NSUTF8StringEncoding error:nil];
+            
+            // Check the result from database
+            if ([postResult isEqualToString:@"Cannot update your password! Your current password is invalid."]) {
+                UIAlertView *failAlert = [[UIAlertView alloc] initWithTitle:@"Fail to update password" message:postResult delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+                [failAlert show];
+                return;
+                
+            } else {
+                UIAlertView *successfulAlert = [[UIAlertView alloc] initWithTitle:@"Successfully!" message:postResult delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+                [successfulAlert show];
+                return;
+                
+                NSLog(@"Username has been created successfully.");
+            }// End if-else Check result from database.
+            
+        }// End if-else When all the inputs seem right.
 
-    }
-}
+    } // End Check if all required fields have been filled.
+    
+     
+}// End Change Password method
 
     
 
